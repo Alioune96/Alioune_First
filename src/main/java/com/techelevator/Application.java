3578f18269c3e9c89ca7f9db499b6129717d2de7
Alioune Diasse <aliounediasse1996@yahoo.com>
2024-06-06 14:08:15 -0400

    itemcreated

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -2,19 +2,21 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-
+import java.util.*;
 
 
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		double totalSales = 0;
 		boolean isOn = true;
 		double machineBalance = 0;
 		File logFile;
 		File salesFile;
+		SortedMap<String,List<Item>> vendingMachineItems= new TreeMap<>();
+
+
 
 		{
 			logFile = new File("C:\\Users\\Student\\workspace\\capstonewithDing\\java-blue-minicapstonemodule1-team3\\src\\main\\resources\\log.txt");
@@ -22,10 +24,44 @@ public class Application {
 
 
 		}
+		Scanner keyboard = new Scanner(System.in);
+		File trycase = new File("vendingmachine.csv");
+		Scanner readFile = new Scanner(trycase);
 
-		Scanner readFile = new Scanner("vendingmachine.csv");
-		String fileinfo = readFile.nextLine();
-		String [] splitfileKeys = fileinfo.split("|");
+		while(readFile.hasNext()){
+
+			String fileinfo = readFile.nextLine();
+
+			List<Item> elevenEachItem = new ArrayList<>();
+			String [] splitfileKeys = fileinfo.trim().split("\\|");
+
+			String mapKeys = splitfileKeys[0];
+			String itemname = splitfileKeys[1];
+			double itemPrice = Double.valueOf(splitfileKeys[2]);
+			String itemType = splitfileKeys[3];
+
+			Item chipsTypeOne = new Item(itemname,itemPrice,itemType);
+
+			for (int i = 0; i < 11; i++) {
+				elevenEachItem.add(chipsTypeOne);
+			}
+
+			vendingMachineItems.put(mapKeys,elevenEachItem);
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		while (isOn){
 
