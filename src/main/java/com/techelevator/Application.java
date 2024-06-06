cbd3c9453fa2661ddcfda6744a7c78546a2f3b67
Alioune Diasse <aliounediasse1996@yahoo.com>
2024-06-06 12:16:19 -0400

    updated

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -1,11 +1,33 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) {
-		File logFile = new File("C:\\Users\\Student\\workspace\\capstonewithDing\\java-blue-minicapstonemodule1-team3\\src\\main\\resources\\log.txt");
-		File salesFile = new File("C:\\Users\\Student\\workspace\\capstonewithDing\\java-blue-minicapstonemodule1-team3\\src\\main\\resources\\finalReport.txt");
+		double totalSales = 0;
+		boolean isOn = true;
+		double machineBalance = 0;
+		File logFile;
+		File salesFile;
+
+		{
+			logFile = new File("C:\\Users\\Student\\workspace\\capstonewithDing\\java-blue-minicapstonemodule1-team3\\src\\main\\resources\\log.txt");
+			salesFile = new File("C:\\Users\\Student\\workspace\\capstonewithDing\\java-blue-minicapstonemodule1-team3\\src\\main\\resources\\finalReport.txt");
+
+
+		}
+
+		Scanner readFile = new Scanner("vendingmachine.csv");
+		String fileinfo = readFile.nextLine();
+		String [] splitfileKeys = fileinfo.split("|");
+
+		while (isOn){
+
+
+		}
+
 	}
 }
